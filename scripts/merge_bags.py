e01e8d78acd4cf661085fe9c3c539a181e5c4117
Zichao Zhang <zhangzichao17@gmail.com>
2015-01-07 19:25:47 +0100

    added parameters parse

--- scripts/merge_bags.py
+++ scripts/merge_bags.py
@@ -5,8 +5,21 @@ import argparse
 import rosbag
 import rospy
 
-def merge_bags():
-    print "merge bags"
+def merge_bags(input_bags, merged_bag):
+    print "Merging", input_bags, "into", merged_bag 
 
 if __name__ == '__main__':
-    merge_bags()
+    parser = argparse.ArgumentParser(description='''
+    merge multiple bag files
+    ''')
+    parser.add_argument('input_bags',
+            type=str,
+            nargs='*',
+            help='file contains pose data')
+    parser.add_argument('--merged_bag',
+            type=str,
+            default='merged.bag',
+            help="name of merged bag file"
+            )
+    args = parser.parse_args()
+    merge_bags(args.input_bags, args.merged_bag)
