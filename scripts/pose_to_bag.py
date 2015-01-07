af18737440da68c7a535a9bba6d7876ec2439892
Zichao Zhang <zhangzichao17@gmail.com>
2015-01-07 16:50:59 +0100

    covert pose to ROS message

--- scripts/pose_to_bag.py
+++ scripts/pose_to_bag.py
@@ -15,8 +15,24 @@ def pose_to_bag(file):
     full_name = os.path.realpath(file)
     output_file = os.path.join(os.path.dirname(full_name),
             os.path.splitext(os.path.basename(full_name))[0]+'.bag') 
+    bag = rosbag.Bag(output_file, 'w')
 
-    print full_name, output_file
+    pose_data = np.loadtxt(full_name)
+    base_sec = rospy.Time.now().to_sec()
+
+    for row in pose_data:
+        pose = Pose()
+        pose.position = Point(row[1], row[2], row[3])
+        pose.orientation = Quaternion(row[5], row[6], row[7], row[4])
+
+        # time
+        rel_sec = row[0]
+        bag_sec = rospy.Time.from_sec(base_sec+rel_sec)
+
+        # write
+        bag.write('pose', pose, bag_sec)
+
+    bag.close()
 
 
 if __name__ == '__main__':
