812733c9d6296356f8b9fd6dfdff44db1bf89b7e
Zichao Zhang <zhangzichao17@gmail.com>
2015-01-07 19:36:25 +0100

    check whether bag files exist

--- scripts/merge_bags.py
+++ scripts/merge_bags.py
@@ -6,7 +6,22 @@ import rosbag
 import rospy
 
 def merge_bags(input_bags, merged_bag):
+    rospy.init_node("merge_bags")
+
+    for bag in input_bags:
+        try:
+            _check_bag_file(bag)
+        except Exception as e:
+            raise e
+    output_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+            merged_bag)
+
     print "Merging", input_bags, "into", merged_bag 
+
+
+def _check_bag_file(bag):
+    if not os.path.exists(bag):
+        raise ValueError("Files not exist")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='''
