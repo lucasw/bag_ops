6e8beaa5f472c9e61bb755d21ffe89e82da61caf
Zichao Zhang <zhangzichao17@gmail.com>
2015-01-07 15:41:32 +0100

    set up package structure

---
+++ scripts/pose_to_bag.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+import argparse
+
+import numpy as np
+from bag_ops import *
+#import bag_ops.transformations as tf
+import rosbag
+import rospy
+from geometry_msgs.msg import Pose, Point, Quaternion
+
+def pose_to_bag(file):
+    print 'pose_to_bag'
+
+
+if __name__ == '__main__':
+    pose_to_bag('test')
