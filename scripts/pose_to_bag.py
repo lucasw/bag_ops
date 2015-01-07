35a39e01c8c111110e7b4e64fdb55fa8a15a81b2
Zichao Zhang <zhangzichao17@gmail.com>
2015-01-07 19:00:05 +0100

    add euler conversion if data lenght is 7
    euler conversion not tested

--- scripts/pose_to_bag.py
+++ scripts/pose_to_bag.py
@@ -9,7 +9,7 @@ import rosbag
 import rospy
 from geometry_msgs.msg import Pose, Point, Quaternion
 
-def pose_to_bag(file, topic):
+def pose_to_bag(file, topic, axies):
     rospy.init_node("pose_to_bag")
 
     full_name = os.path.realpath(file)
@@ -22,21 +22,29 @@ def pose_to_bag(file, topic):
 
     try:
         _check_data(pose_data)
+        for row in pose_data:
+            pose = _row_to_pose(row, axies)
+            bag_sec = rospy.Time.from_sec(base_sec+row[0])
+            bag.write(topic, pose, bag_sec)
     except Exception as e:
         raise e
 
-    for row in pose_data:
-        pose = _row_to_pose(row)
-        bag_sec = rospy.Time.from_sec(base_sec+row[0])
-        bag.write(topic, pose, bag_sec)
-
     bag.close()
 
-def _row_to_pose(row):
+# the order of transformation output: w x y z
+# the order of ROS: x y z w
+def _row_to_pose(row, axies):
     pose = Pose()
     pose.position = Point(row[1], row[2], row[3])
-    pose.orientation = Quaternion(row[5], row[6], row[7], row[4])
-
+    if row.shape[0] == 8:
+        pose.orientation = Quaternion(row[4], row[5], row[6], row[7])
+    elif row.shape[0] == 7:
+        if axies == '':
+            raise ValueError('The type of euler should be specified')
+        quat = tf.quaternion_from_euler(row[4], row[5], row[6], axies)
+        pose.orientation = Quaternion(quat[1], quat[2], quat[3], quat[0])
+    else:
+        raise TypeError('The lenght of the array should be 7 or 8')
     return pose
 
 def _check_data(pose_data):
@@ -54,5 +62,14 @@ if __name__ == '__main__':
     parser.add_argument('topic',
             type=str,
             help='topic name to write to')
+    parser.add_argument('--axies',
+            type=str,
+            default='',
+            help="""if use euler angle, specify the type 
+              Axes 4-string: e.g. 'sxyz' or 'ryxy'
+              first character : rotations are applied to 's'tatic or 'r'otating frame
+              remaining characters : successive rotation axis 'x', 'y', or 'z'
+            """
+            )
     args = parser.parse_args()
-    pose_to_bag(args.file, args.topic)
+    pose_to_bag(args.file, args.topic, args.axies)
