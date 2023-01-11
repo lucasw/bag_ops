486b96ee0cc7cd7445d27cfdbd3d8741c98048ff
Lucas Walter <wsacul@gmail.com>
2023-01-11 07:06:07 -0800

    write bytes from data field to file

--- scripts/bag_bytes.py
+++ scripts/bag_bytes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-# print out the first num_bytes in a 'data' field of all messages in a bag
+# Lucas Walter
+# output the bytes in a 'data' field of all messages in a bag on a given topic to a file
 
 import sys
 
@@ -7,18 +8,22 @@ import rosbag
 import rospy
 
 
-if __name__ == "__main__":
-    rospy.init_node("bag_data_bytes")
-    argv = [arg for arg in sys.argv[1:] if not arg.startswith("_")]
-
-    num_bytes_to_print = rospy.get_param("~num_bytes", 24)
-
-    for input_bag in argv:
-        rospy.loginfo(f"opening {input_bag}")
-        for topic, msg, t in rosbag.Bag(input_bag).read_messages():
-            if rospy.is_shutdown():
-                break
-            if hasattr(msg, 'data'):
+def get_bytes(input_bags, out_file, selected_topic, verbose=False, num_bytes_to_print=0):
+    rospy.loginfo(f"opening {input_bag}")
+    count = 0
+    num_bytes = 0
+    for topic, msg, t in rosbag.Bag(input_bag).read_messages():
+        if topic is not None and topic != selected_topic:
+            # rospy.loginfo_throttle(0.0, f"{topic} != {selected_topic}")
+            continue
+        if rospy.is_shutdown():
+            break
+        if hasattr(msg, 'data'):
+            out_file.write(msg.data)
+            num_bytes += len(msg.data)
+            count += 1
+            rospy.loginfo_throttle(2.0, f"{num_bytes}")
+            if num_bytes_to_print > 0:
                 text = ""  # f"{t.to_sec():0.3f} {topic}:"
                 for ind, byte in enumerate(msg.data):
                     if ind >= num_bytes_to_print:
@@ -27,3 +32,19 @@ if __name__ == "__main__":
                     if ind % 8 == 7:
                         text += " "
                 print(text)
+    rospy.loginfo(f"{num_bytes} written from {count} messages")
+    return num_bytes
+
+
+if __name__ == "__main__":
+    rospy.init_node("bag_data_bytes")
+    topic = rospy.get_param("~topic", "")
+    out_filename = rospy.get_param("~out_filename", "bytes.bin")
+    argv = [arg for arg in sys.argv[1:] if not arg.startswith("_")]
+
+    num_bytes_to_print = rospy.get_param("~num_bytes", 0)
+
+    with open(out_filename, "wb") as out_file:
+        rospy.loginfo(f"writing to {out_filename}")
+        for input_bag in argv:
+            get_bytes(input_bag, out_file, topic, num_bytes_to_print)
