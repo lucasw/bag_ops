55bc5934b3698c6478775bd5cf73f81210b6b971
Lucas Walter <wsacul@gmail.com>
2023-01-24 14:39:34 -0800

    also take messages with 'value' field instead of just those with 'data' in bag_bytes.py

--- scripts/bag_bytes.py
+++ scripts/bag_bytes.py
@@ -8,8 +8,8 @@ import rosbag
 import rospy
 
 
-def get_bytes(input_bags, out_file, selected_topic, verbose=False, num_bytes_to_print=0):
-    rospy.loginfo(f"opening {input_bag}")
+def get_bytes(input_bag, out_file, selected_topic, verbose=False, num_bytes_to_print=0):
+    rospy.loginfo(f"opening {input_bag} {out_file} {selected_topic} {num_bytes_to_print}")
     count = 0
     num_bytes = 0
     for topic, msg, t in rosbag.Bag(input_bag).read_messages():
@@ -18,20 +18,32 @@ def get_bytes(input_bags, out_file, selected_topic, verbose=False, num_bytes_to_
             continue
         if rospy.is_shutdown():
             break
+        data = None
         if hasattr(msg, 'data'):
-            out_file.write(msg.data)
-            num_bytes += len(msg.data)
+            data = msg.data
+        elif hasattr(msg, 'value'):
+            value = msg.value
+            text = ""
+            data = value.to_bytes(4, "big")
+            for byte in data:
+                text += f" 0x{byte:02x}"
+            text += f" {value}"
+            # print(text)
+        if data is not None:
+            out_file.write(data)
+            num_bytes += len(data)
             count += 1
             rospy.loginfo_throttle(2.0, f"{num_bytes}")
+            # print(f"test {num_bytes_to_print}")
             if num_bytes_to_print > 0:
                 text = ""  # f"{t.to_sec():0.3f} {topic}:"
-                for ind, byte in enumerate(msg.data):
+                for ind, byte in enumerate(data):
                     if ind >= num_bytes_to_print:
                         break
-                    text += f" {byte:02x}"
+                    text += f" 0x{byte:02x}"
                     if ind % 8 == 7:
                         text += " "
-                print(text)
+                rospy.loginfo_throttle(1.0, text)
     rospy.loginfo(f"{num_bytes} written from {count} messages")
     return num_bytes
 
@@ -39,12 +51,12 @@ def get_bytes(input_bags, out_file, selected_topic, verbose=False, num_bytes_to_
 if __name__ == "__main__":
     rospy.init_node("bag_data_bytes")
     topic = rospy.get_param("~topic", "")
+    num_bytes_to_print = rospy.get_param("~num_bytes", 0)
+    print(num_bytes_to_print)
     out_filename = rospy.get_param("~out_filename", "bytes.bin")
     argv = [arg for arg in sys.argv[1:] if not arg.startswith("_")]
-
-    num_bytes_to_print = rospy.get_param("~num_bytes", 0)
 
     with open(out_filename, "wb") as out_file:
         rospy.loginfo(f"writing to {out_filename}")
         for input_bag in argv:
-            get_bytes(input_bag, out_file, topic, num_bytes_to_print)
+            get_bytes(input_bag, out_file, topic, num_bytes_to_print=num_bytes_to_print)
